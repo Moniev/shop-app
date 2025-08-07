c1851b8568fef301448421c534254a2fdb94d5b5
Moniev <m0ni3v@gmail.com>
2025-08-07 19:29:14 +0200

    added configs for startup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,17 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_caching = false
+  config.action_mailer.smtp_settings = {
+    address: ENV.fetch('SMTP_ADDRESS'),
+    port: ENV.fetch('SMTP_PORT', 587),
+    user_name: ENV.fetch('SMTP_USERNAME'),
+    password: ENV.fetch('SMTP_PASSWORD'),
+    domain: ENV.fetch('SMTP_DOMAIN'),
+    authentication: :plain,
+    enable_starttls_auto: true,
+    openssl_verify_mode: 'peer'
+  }
 end
